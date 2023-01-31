0520d54155003d5fc87453ab07b02a6f45d34f69
AlexseyLoginov <devileean@gmail.com>
2023-01-31 22:26:44 +0300

    task_001 complete

---
+++ Python_Lectures_GB/Lection001/task_001.py
@@ -0,0 +1,10 @@
+# Задача 1: Посчитайте сумму трёх введённых чисел.
+# 1 + 2 + 3 = 6
+
+number1 = int(input('введите первое чесло: '))
+number2 = int(input('введите второе чесло: '))
+number3 = int(input('введите третье чесло: '))
+
+summa = number1 + number2 + number3
+
+print(f'{number1} + {number2} + {number3} = {summa}')
