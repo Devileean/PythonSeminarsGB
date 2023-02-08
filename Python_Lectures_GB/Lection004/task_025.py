62be3778e02e4c1afd8989ca046a2987e9e988c1
AlexseyLoginov <devileean@gmail.com>
2023-02-08 23:21:26 +0300

    task 25 complete

---
+++ Python_Lectures_GB/Lection004/task_025.py
@@ -0,0 +1,16 @@
+print('Задача 25. \n'
+'Напишите программу, которая принимает на вход строку, и отслеживает, сколько раз каждый символ \n '
+'уже встречался. Количество повторов добавляется к символам с помощью постфикса формата _n. \n')
+
+line = [i for i in input().split()]
+letters, new_line = {}, []
+for letter in set(line):
+    letters.setdefault(letter, 0)
+for s in line:
+    if letters[s] == 0:
+        new_line.append(s)
+        letters[s] += 1
+    else:
+        new_line.append(f'{s}_{letters[s]}')
+        letters[s] += 1
+print(*new_line)
