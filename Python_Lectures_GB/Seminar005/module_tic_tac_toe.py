aa1f6e48ff6f4544a95c48c2ef356e74789e0c19
AlexseyLoginov <devileean@gmail.com>
2023-02-22 23:22:20 +0300

    tic tac toe and teris

--- Python_Lectures_GB/Seminar005/module_tic_tac_toe.py
+++ Python_Lectures_GB/Seminar005/module_tic_tac_toe.py
@@ -17,6 +17,19 @@ def mytictactoe(val):
     print("\n")
 
 
+# Defining Function to check Victory
+def check_Victory(playerpos, curplayer):
+    # All probable winning combinations
+    solution = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+
+    # Loop to check whether any winning combination is satisfied or not
+    for i in solution:
+        if all(j in playerpos[curplayer] for j in i):
+            # Return True if any winning combination is satisfied
+            return True
+            # Return False if no combination is satisfied
+    return False
+
 # Function for a single game(функция для одиночной игры) of Tic-Tac-Toe
 def singlegame(curplayer):
     # Representing the Tic-Tac-Toe
@@ -24,6 +37,25 @@ def singlegame(curplayer):
 
     # Storing the positions occupied by X and O
     playerpos = {'X': [], 'O': []}
+
+# Defining Function to check if the game is Tied
+def check_Tie(playerpos):
+    if len(playerpos['X']) + len(playerpos['O']) == 9:
+        return True
+    return False
+
+
+def myscoreboard(scoreboard):
+    print("\t--------------------------------")
+    print("\t         SCORE BOARD       ")
+    print("\t--------------------------------")
+
+    listofplayers = list(scoreboard.keys())
+    print("\t   ", listofplayers[0], "\t    ", scoreboard[listofplayers[0]])
+    print("\t   ", listofplayers[1], "\t    ", scoreboard[listofplayers[1]])
+
+    print("\t--------------------------------\n")
+
 
 
 
